47cd6091f953b33699e587fabee7fde79bf256a3
nancybenjwal <benjwalnancy@gmail.com>
2025-12-05 22:44:53 +0530

    added vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/pumbas-love-scroll/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
